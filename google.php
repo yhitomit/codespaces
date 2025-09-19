86703301ae882c9c009ca1c7938594d59c01e3af
yhitomit <timothycmathews@outlook.com>
2025-09-19 13:54:53 +0000

    added Google file

---
+++ google.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Google</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div id="header">
+            <div class="header-column header-left">
+                <a href="#">About</a>
+                <a href="#">Store</a>
+            </div>
+            <div class="header-column header-right">
+                <a href="#">Gmail</a>
+                <a href="#">Images</a>
+                <a href="#" class="header-icon header-labs"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 11 4 A 1.0001 1.0001 0 1 0 11 6 L 11 12 L 11 13 L 11 14 L 4.4511719 22.734375 L 4.4511719 22.736328 A 2 2 0 0 0 4 24 A 2 2 0 0 0 6 26 L 15 26 L 24 26 A 2 2 0 0 0 26 24 A 2 2 0 0 0 25.548828 22.736328 A 2 2 0 0 0 25.548828 22.734375 L 19 14 L 19 13 L 19 12 L 19 6 A 1.0001 1.0001 0 1 0 19 4 L 11 4 z M 13 6 L 17 6 L 17 12 L 13 12 L 13 6 z M 14 14 C 14.552 14 15 14.448 15 15 C 15 15.552 14.552 16 14 16 C 13.448 16 13 15.552 13 15 C 13 14.448 13.448 14 14 14 z M 17.5 20 C 18.328 20 19 20.672 19 21.5 C 19 22.328 18.328 23 17.5 23 C 16.672 23 16 22.328 16 21.5 C 16 20.672 16.672 20 17.5 20 z"/></svg></a>
+                <a href="#" class="header-icon header-menu"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 7 4.5 A 2.5 2.5 0 0 0 4.5 7 A 2.5 2.5 0 0 0 7 9.5 A 2.5 2.5 0 0 0 9.5 7 A 2.5 2.5 0 0 0 7 4.5 z M 15 4.5 A 2.5 2.5 0 0 0 12.5 7 A 2.5 2.5 0 0 0 15 9.5 A 2.5 2.5 0 0 0 17.5 7 A 2.5 2.5 0 0 0 15 4.5 z M 23 4.5 A 2.5 2.5 0 0 0 20.5 7 A 2.5 2.5 0 0 0 23 9.5 A 2.5 2.5 0 0 0 25.5 7 A 2.5 2.5 0 0 0 23 4.5 z M 7 12.5 A 2.5 2.5 0 0 0 4.5 15 A 2.5 2.5 0 0 0 7 17.5 A 2.5 2.5 0 0 0 9.5 15 A 2.5 2.5 0 0 0 7 12.5 z M 15 12.5 A 2.5 2.5 0 0 0 12.5 15 A 2.5 2.5 0 0 0 15 17.5 A 2.5 2.5 0 0 0 17.5 15 A 2.5 2.5 0 0 0 15 12.5 z M 23 12.5 A 2.5 2.5 0 0 0 20.5 15 A 2.5 2.5 0 0 0 23 17.5 A 2.5 2.5 0 0 0 25.5 15 A 2.5 2.5 0 0 0 23 12.5 z M 7 20.5 A 2.5 2.5 0 0 0 4.5 23 A 2.5 2.5 0 0 0 7 25.5 A 2.5 2.5 0 0 0 9.5 23 A 2.5 2.5 0 0 0 7 20.5 z M 15 20.5 A 2.5 2.5 0 0 0 12.5 23 A 2.5 2.5 0 0 0 15 25.5 A 2.5 2.5 0 0 0 17.5 23 A 2.5 2.5 0 0 0 15 20.5 z M 23 20.5 A 2.5 2.5 0 0 0 20.5 23 A 2.5 2.5 0 0 0 23 25.5 A 2.5 2.5 0 0 0 25.5 23 A 2.5 2.5 0 0 0 23 20.5 z"/></svg></a>
+                <a href="#" class="header-profile">User</a>
+            </div>
+        </div>
+        <div id="main">
+            <div class="google-logo">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 272 92" width="272" height="92"><path fill="#EA4335" d="M115.75 47.18c0 12.77-9.99 22.18-22.25 22.18s-22.25-9.41-22.25-22.18C71.25 34.32 81.24 25 93.5 25s22.25 9.32 22.25 22.18zm-9.74 0c0-7.98-5.79-13.44-12.51-13.44S80.99 39.2 80.99 47.18c0 7.9 5.79 13.44 12.51 13.44s12.51-5.55 12.51-13.44z"/><path fill="#FBBC05" d="M163.75 47.18c0 12.77-9.99 22.18-22.25 22.18s-22.25-9.41-22.25-22.18c0-12.85 9.99-22.18 22.25-22.18s22.25 9.32 22.25 22.18zm-9.74 0c0-7.98-5.79-13.44-12.51-13.44s-12.51 5.46-12.51 13.44c0 7.9 5.79 13.44 12.51 13.44s12.51-5.55 12.51-13.44z"/><path fill="#4285F4" d="M209.75 26.34v39.82c0 16.38-9.66 23.07-21.08 23.07-10.75 0-17.22-7.19-19.66-13.07l8.48-3.53c1.51 3.61 5.21 7.87 11.17 7.87 7.31 0 11.84-4.51 11.84-13v-3.19h-.34c-2.18 2.69-6.38 5.04-11.68 5.04-11.09 0-21.25-9.66-21.25-22.09 0-12.52 10.16-22.26 21.25-22.26 5.29 0 9.49 2.35 11.68 4.96h.34v-3.61h9.25zm-8.56 20.92c0-7.81-5.21-13.52-11.84-13.52-6.72 0-12.35 5.71-12.35 13.52 0 7.73 5.63 13.36 12.35 13.36 6.63 0 11.84-5.63 11.84-13.36z"/><path fill="#34A853" d="M225 3v65h-9.5V3h9.5z"/><path fill="#EA4335" d="M262.02 54.48l7.56 5.04c-2.44 3.61-8.32 9.83-18.48 9.83-12.6 0-22.01-9.74-22.01-22.18 0-13.19 9.49-22.18 20.92-22.18 11.51 0 17.14 9.16 18.98 14.11l1.01 2.52-29.65 12.28c2.27 4.45 5.8 6.72 10.75 6.72 4.96 0 8.4-2.44 10.92-6.14zm-23.27-7.98l19.82-8.23c-1.09-2.77-4.37-4.7-8.23-4.7-4.95 0-11.84 4.37-11.59 12.93z"/><path fill="#4285F4" d="M35.29 41.41V32H67c.31 1.64.47 3.58.47 5.68 0 7.06-1.93 15.79-8.15 22.01-6.05 6.3-13.78 9.66-24.02 9.66C16.32 69.35.36 53.89.36 34.91.36 15.93 16.32.47 35.3.47c10.5 0 17.98 4.12 23.6 9.49l-6.64 6.64c-4.03-3.78-9.49-6.72-16.97-6.72-13.86 0-24.7 11.17-24.7 25.03 0 13.86 10.84 25.03 24.7 25.03 8.99 0 14.11-3.61 17.39-6.89 2.66-2.66 4.41-6.46 5.1-11.65l-22.49.01z"/></svg>
+            </div>
+            <form class="search-form" action="#" method="GET">
+                <div class="search-box">
+                    <svg id="search-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/></svg>
+                    <input type="text" name="q" placeholder="" />
+                    <svg id="microphone-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="30px" height="30px">    <path d="M 12 2 C 10.343 2 9 3.343 9 5 L 9 11 C 9 12.657 10.343 14 12 14 C 13.657 14 15 12.657 15 11 L 15 5 C 15 3.343 13.657 2 12 2 z M 6.0878906 11 C 5.4818906 11 4.9937969 11.537719 5.0917969 12.136719 C 5.5816755 15.136436 7.9811339 17.488992 11 17.921875 L 11 20 C 11 20.552 11.448 21 12 21 C 12.552 21 13 20.552 13 20 L 13 17.921875 C 16.018866 17.488992 18.418325 15.136436 18.908203 12.136719 C 19.006203 11.537719 18.518109 11 17.912109 11 C 17.418109 11 17.010734 11.363563 16.927734 11.851562 C 16.522734 14.206563 14.471 16 12 16 C 9.529 16 7.4772656 14.206563 7.0722656 11.851562 C 6.9892656 11.363563 6.5828906 11 6.0878906 11 z"/></svg>
+                    <svg id="camera-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 19.09375 5 C 18.011719 5 17.105469 5.625 16.5625 6.4375 C 16.5625 6.449219 16.5625 6.457031 16.5625 6.46875 L 14.96875 9 L 6 9 C 3.253906 9 1 11.253906 1 14 L 1 38 C 1 40.746094 3.253906 43 6 43 L 44 43 C 46.746094 43 49 40.746094 49 38 L 49 14 C 49 11.253906 46.746094 9 44 9 L 34.9375 9 L 33.34375 6.46875 C 33.34375 6.457031 33.34375 6.449219 33.34375 6.4375 C 32.800781 5.625 31.894531 5 30.8125 5 Z M 19.09375 7 L 30.8125 7 C 31.132813 7 31.398438 7.175781 31.65625 7.5625 L 33.5625 10.53125 C 33.746094 10.820313 34.0625 11 34.40625 11 L 44 11 C 45.65625 11 47 12.34375 47 14 L 47 38 C 47 39.65625 45.65625 41 44 41 L 6 41 C 4.34375 41 3 39.65625 3 38 L 3 14 C 3 12.34375 4.34375 11 6 11 L 15.5 11 C 15.84375 11 16.160156 10.820313 16.34375 10.53125 L 18.21875 7.5625 L 18.25 7.53125 C 18.5 7.179688 18.789063 7 19.09375 7 Z M 10 13 C 8.355469 13 7 14.355469 7 16 C 7 17.644531 8.355469 19 10 19 C 11.644531 19 13 17.644531 13 16 C 13 14.355469 11.644531 13 10 13 Z M 10 15 C 10.554688 15 11 15.445313 11 16 C 11 16.554688 10.554688 17 10 17 C 9.445313 17 9 16.554688 9 16 C 9 15.445313 9.445313 15 10 15 Z M 25 15 C 18.9375 15 14 19.9375 14 26 C 14 32.0625 18.9375 37 25 37 C 31.0625 37 36 32.0625 36 26 C 36 19.9375 31.0625 15 25 15 Z M 25 17 C 29.980469 17 34 21.019531 34 26 C 34 30.980469 29.980469 35 25 35 C 20.019531 35 16 30.980469 16 26 C 16 21.019531 20.019531 17 25 17 Z"/></svg>
+                </div>
+                <div class="search-buttons">
+                    <button type="submit" class="search-btn">Google Search</button>
+                    <button type="button" class="lucky-btn">I'm Feeling Lucky</button>
+                </div>
+            </form>
+        </div>
+        <div id="footer">
+            <div class="footer-column footer-left">
+                <a href="#">Advertising</a>
+                <a href="#">Business</a>
+                <a href="#">How Search Works</a>
+            </div>
+            <div class="footer-column footer-right">
+                <a href="#">Privacy</a>
+                <a href="#">Terms</a>
+                <a href="#">Settings</a>
+            </div>
+        </div>
+    </body>
+    <style>
+        body {
+            background: #202124;
+            color: #fff;
+            font-family: "Host Grotesk", sans-serif;
+            margin: 0;
+            font-size: 16px;
+            overflow: hidden;
+        }
+        div#header {
+            display: grid;
+            grid: auto-flow dense / 1fr 1fr;
+            padding: 10px 20px;
+            font-size: 14px;
+        }
+        .header-column.header-right {
+            display: flex;
+            justify-content: flex-end;
+        }
+        a {
+            text-decoration: none;
+            color: #fff;
+        }
+        div#header .header-column {
+            display: flex;
+            align-items: center;
+            grid-gap: 15px;
+        }
+        #header svg {
+            width: 25px;
+        }
+        #header a.header-icon {
+            background: none;
+            border-radius: 100%;
+            height: 35px;
+            width: 35px;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            transition: all 0.3s ease;
+            -webkit-transition: all 0.3s ease;
+            -moz-transition: all 0.3s ease;
+        }
+        #header a.header-icon:hover {
+            background: #4d5156;
+        }
+        #header a.header-icon svg path {
+            fill: #fff;
+        }
+        div#main {
+            height: calc(100vh - 110px);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        form.search-form {
+            margin-bottom: 100px;
+            width: 100%;
+            max-width: 600px;
+        }
+        form.search-form .search-box {
+            background: #4d5156;
+            border-radius: 30px;
+        }
+        form.search-form .search-box svg path {
+            fill: #fff;
+        }
+        form.search-form .search-box svg {
+            height: 20px;
+        }
+        form.search-form .search-box input {
+            height: 45px;
+            background: none;
+            border: none;
+            color: #fff;
+            outline: none;
+            font-size: 16px;
+        }
+        form.search-form .search-box {
+            display: flex;
+            align-items: center;
+        }
+        form.search-form .search-box svg, form.search-form .search-box input {
+            flex: 1;
+        }
+        svg#search-icon,
+        svg#camera-icon,
+        svg#microphone-icon {
+            max-width: 45px;
+        }
+        svg#camera-icon {
+            margin-right: 20px;
+        }
+        .search-buttons {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 30px 0 0;
+            grid-gap: 15px;
+        }
+        .search-buttons button {
+            background: #303134;
+            color: #fff;
+            border: 1px solid #303134;
+            border-radius: 6px;
+            font-size: 14px;
+            padding: 7px 15px;
+            cursor: pointer;
+        }
+        .search-buttons button:hover {
+            border-color: #4d5156;
+            transition: all 0.3s ease;
+            -moz-transition: all 0.3s ease;
+            -o-transition: all 0.3s ease;
+        }
+        div#footer {
+            height: 55px;
+            display: grid;
+            grid: auto-flow dense / 1fr 1fr;
+            background: #171717;
+            padding: 0 20px;
+            font-size: 14px;
+        }
+        div#footer .footer-column {
+            display: flex;
+            align-items: center;
+            grid-gap: 15px;
+        }
+        div#footer .footer-column.footer-right {
+            display: flex;
+            justify-content: flex-end;
+        }
+    </style>
+</html>
