b14e2ef3f232ed00f250e381399015ae25b3a904
yhitomit <timothycmathews@outlook.com>
2025-10-10 14:02:05 +0000

    starting to build youtube header

---
+++ youtube.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>YouTube</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/assets/youtube.css" media="all" />
+    </head>
+    <body>
+        <!-- Get started here -->
+         <div id="header">
+            <div class="header-inner header-left">
+                <img id="logo" src="/assets/youtube.svg" />
+            </div>
+            <div class="header-inner header-center">
+                <form method="post" id="search">
+                    <input type="text" name="search" placeholders="Search" />
+                    <input type="submit" value="Search" />
+                </form> 
+            </div>
+            <div class="header-inner header-right">
+            </div>
+         </div>
+        <!-- https://picsum.photos/530/300 -->
+    </body>
+</html>
